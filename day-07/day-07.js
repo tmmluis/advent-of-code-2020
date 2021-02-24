c9a94b83235dbe256e2d4f1ff58b07dbabfaa772
tmmluis <tmmluis@gmail.com>
2021-02-24 18:16:34 +0100

    solve day 7 part one

---
+++ day-07/day-07.js
@@ -0,0 +1,10 @@
+const fs = require('fs')
+const one = require('./one')
+
+fs.readFile('./day-07/input.txt', (err, data) => {
+  if (err) throw err
+  const input = data.toString()
+
+  console.log('Solution Part One:', one(input))
+
+})
--- day-07/one.js
+++ day-07/one.js
@@ -1,5 +1,44 @@
-const one = (input) => {
+const { get } = require("lodash")
 
+const one = (input) => {
+  const rules = input.split(/\r?\n/)
+  const bags = new Map()
+  const validBags = []
+
+  rules.forEach(rule => {
+    // Parse each rule to get an outer bag and the array of inner bags it can contain.
+    const outerBag = rule.slice(0, rule.indexOf('bags') - 1)
+    const innerBags = rule.match(/(?<=\d )(\w|| )+(?= bags?(,||.))/g)
+
+    if (innerBags !== null) {
+      if (innerBags.includes('shiny gold')) {
+        validBags.push(outerBag)
+      } else {
+        bags.set(outerBag, innerBags)
+      }
+    }
+  })
+
+  for (let i = 0; i < validBags.length; i++) {
+    let bag = validBags[i]
+    let outerBag = getOuterBag(bags, bag)
+    if (outerBag) {
+      bags.delete(outerBag)
+      validBags.push(outerBag)
+      i = -1
+    }
+  }
+
+  return validBags.length
+}
+
+const getOuterBag = (bags, bag) => {
+  for (let [outerBag, innerBags] of bags) {
+    if (innerBags.includes(bag)) {
+      return outerBag
+    }
+  }
+  return null
 }
 
 module.exports = one
